17e7a35b5601eb617a596099164c71698eead952
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2024-07-02 00:55:23 +0700

    Tạo router cơ bản

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("ok");
+import express,{ Express , Request , Response} from "express";
+
+const app: Express = express();
+const port: number | String = process.env.PORT || 3000;
+
+app.use("/topics",(req: Request, res: Response)=>{
+    res.send("Chủ đề bài hát");
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
