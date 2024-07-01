13534aa44c78a6444b9f95faa75f62619af6bec7
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2024-07-02 01:05:07 +0700

    Cài đặt PUG

--- index.ts
+++ index.ts
@@ -3,8 +3,11 @@ import express,{ Express , Request , Response} from "express";
 const app: Express = express();
 const port: number | String = process.env.PORT || 3000;
 
+app.set("views","./views");
+app.set("view engine","pug");
+
 app.use("/topics",(req: Request, res: Response)=>{
-    res.send("Chủ đề bài hát");
+    res.render("client/pages/topics/index.pug");
 })
 
 app.listen(port, () => {
