181fb35ec9f1286f74e77ce7dd268876d7df80a7
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2024-07-02 14:55:42 +0700

    Nhúng file tĩnh

--- index.ts
+++ index.ts
@@ -11,6 +11,8 @@ database.connect();
 const app: Express = express();
 const port: number | String = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.set("views","./views");
 app.set("view engine","pug");
 
