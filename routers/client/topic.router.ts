4986b9151d6bf7cc41f6745b47a7d50fdfa339b9
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2024-07-02 14:17:21 +0700

    Tạo controller

--- routers/client/topic.router.ts
+++ routers/client/topic.router.ts
@@ -1,16 +1,10 @@
 import { Router , Request , Response} from "express";
-import Topic from "../../models/topics.model";
+import * as controller from "../../controllers/client/topic.controller"
+
 const router: Router = Router();
 
 
-router.get("/", async (req: Request, res: Response)=>{
-    const topics = await Topic.find({
-        deleted:false
-    });
-    console.log(topics);
-    
-    res.render("client/pages/topics/index.pug");
-});
+router.get("/", controller.topics );
 
 
 export const topicRouter: Router =  router;
