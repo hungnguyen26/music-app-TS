f01d7f0716e3eacf4a147194831fcc3d44aecf32
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-07-13 21:55:17 +0700

    Trang chủ client

--- dist/routers/client/index.router.js
+++ dist/routers/client/index.router.js
@@ -4,7 +4,9 @@ const topic_router_1 = require("./topic.router");
 const songs_router_1 = require("./songs.router");
 const favorite_songs_router_1 = require("./favorite-songs.router");
 const search_roter_1 = require("./search.roter");
+const home_roter_1 = require("./home.roter");
 const clientRouter = (app) => {
+    app.use("/", home_roter_1.homeRouter);
     app.use("/topics", topic_router_1.topicRouter);
     app.use("/songs", songs_router_1.songsRouter);
     app.use("/favorite-songs", favorite_songs_router_1.favoriteSongRouter);
